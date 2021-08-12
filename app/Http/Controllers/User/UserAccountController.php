cbb661483e46d7a11823f5eb9f294d0151b7adb5
Ignacio Chiaravalle <nachochiara@gmail.com>
2021-08-12 17:12:19 -0300

    Prevented unique teacher manager from self deleting.

--- app/Http/Controllers/User/UserAccountController.php
+++ app/Http/Controllers/User/UserAccountController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\User;
 
 use App\Http\Controllers\Controller;
 use App\Http\Controllers\User\UserManagementController;
+use App\Http\Controllers\GeneralFunctions\ArrayHandlerController;
 use App\Http\Controllers\Teacher\ManageTeachers\TeacherDeletionController;
 use Illuminate\Support\Facades\View;
 use Illuminate\Support\Facades\Auth;
@@ -39,12 +40,25 @@ class UserAccountController extends Controller {
 	protected function deleteSelf() {
 		$user = Auth::user();
 		$userType = $user->type;
-		if ($userType == 'teacher') {
-			$teacher = Teacher::where('name', $user->name)->first();
-			(new TeacherDeletionController)->cascadeDeleteStudents($teacher);
-		}
+		if ($userType == 'teacher' && !$this->handleTeacherDeletion($user))
+			return back()->with('message', "¡No podés eliminar tu cuenta! Sos el único docente que puede administrar a otros docentes. Antes de darte de baja, por favor cedé ese permiso a otro docente del sistema.");
 		(new UserManagementController)->deleteUser($user);
 		return redirect()->route('/login-me-in')->with('success', "Cuenta de usuario eliminada con éxito.");
+	}
+
+	private function handleTeacherDeletion ($user) {
+		$teacher = Teacher::where('name', $user->name)->first();
+		if ($teacher->can_manage_teachers) {
+			$otherManagers = Teacher::where([
+				['name', '!=', $teacher->name],
+				['can_manage_teachers', '=', 'true']
+			])->get();
+			$aHC = new ArrayHandlerController;
+			if ($aHC->findSize($otherManagers) == 0)
+				return false;
+		}
+		(new TeacherDeletionController)->cascadeDeleteStudents($teacher);
+		return true;
 	}
 	
 	private function getUser() {
