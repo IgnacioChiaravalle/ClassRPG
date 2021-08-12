a9e0347e637b3539c4b8dab09df15f8064c2e8a2
Ignacio Chiaravalle <nachochiara@gmail.com>
2021-08-12 17:41:55 -0300

    When a teacher deletes a student, the student's user is only deleted if it has no other teachers. Otherwise, only the Teacher_Student relation is deleted.

--- app/Http/Controllers/Teacher/ManageStudents/StudentUserController.php
+++ app/Http/Controllers/Teacher/ManageStudents/StudentUserController.php
@@ -4,6 +4,9 @@ namespace App\Http\Controllers\Teacher\ManageStudents;
 
 use App\Http\Controllers\Controller;
 use App\Http\Controllers\User\UserManagementController;
+use App\Http\Controllers\GeneralFunctions\ArrayHandlerController;
+use Illuminate\Support\Facades\Auth;
+use App\Models\Teacher_Student;
 use Illuminate\Http\Request;
 
 class StudentUserController extends Controller {
@@ -14,8 +17,22 @@ class StudentUserController extends Controller {
 	}
 
 	protected function deleteStudent($studentName) {
-		$uMC = new UserManagementController;
-		$uMC->deleteUser($uMC->getUserByName($studentName));
+		$otherTeachers = Teacher_Student::where([
+			['student_name', '=', $studentName],
+			['teacher_name', '!=', Auth::user()->name]
+		])->get();
+		$aHC = new ArrayHandlerController;
+		if ($aHC->findSize($otherTeachers) >= 1) {
+			$teacher_student_relation = Teacher_Student::where([
+				['student_name', '=', $studentName],
+				['teacher_name', '=', Auth::user()->name]
+			])->first();
+			$teacher_student_relation->delete();
+		}
+		else {
+			$uMC = new UserManagementController;
+			$uMC->deleteUser($uMC->getUserByName($studentName));
+		}
 		return redirect()->route('/')->with('success', "Alumno eliminado con éxito.");
 	}
 }
